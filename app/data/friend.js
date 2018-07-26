7be4b8dd7a41dd10aa132388afc4db32300734c1
amanda <amandaferraro41@gmail.com>
2018-07-26 19:46:23 -0400

    friendsandhtml

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,7 +1,7 @@
 //arrays that hold all the friend possibilities
 var posFriends = [
     {
-        "name": "Spongebob",
+        "name": "SpongeBob SquarePants",
         "photo": "https://pbs.twimg.com/profile_images/1002272769352978433/9S4QWSR0_400x400.jpg",
         "scores": [
             "5",
@@ -17,10 +17,10 @@ var posFriends = [
         ]
     },
     {
-        "name": "Patrick",
+        "name": "Patrick Star",
         "photo": "https://res.cloudinary.com/teepublic/image/private/s--Tm_-OgRz--/t_Preview/b_rgb:0f7b47,c_limit,f_jpg,h_630,q_90,w_630/v1446161353/production/designs/71233_0.jpg",
         "scores": [
-            "5",
+            "2",
             "1",
             "3",
             "4",
@@ -29,11 +29,11 @@ var posFriends = [
             "4",
             "3",
             "3",
-            "1"
+            "2"
         ]
     },
     {
-        "name": "Squidward",
+        "name": "Squidward Tentacles",
         "photo": "https://www.wikihow.com/images/thumb/c/c1/Draw-Squidward-from-SpongeBob-SquarePants-Step-16.jpg/aid1467847-v4-728px-Draw-Squidward-from-SpongeBob-SquarePants-Step-16.jpg",
         "scores": [
             "3",
@@ -49,7 +49,7 @@ var posFriends = [
         ]
     },
     {
-        "name": "Sandy",
+        "name": "Sandy Cheeks",
         "photo": "https://vignette.wikia.nocookie.net/loveinterest/images/2/2d/Sandycheeks.png/revision/latest?cb=20180419185408",
         "scores": [
             "4",
@@ -61,8 +61,106 @@ var posFriends = [
             "4",
             "5",
             "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Mr. Krabs",
+        "photo": "https://vignette.wikia.nocookie.net/spongebob/images/9/9f/Krabs_artwork.PNG/revision/latest?cb=20171223013253",
+        "scores": [
+            "4",
+            "5",
+            "4",
+            "5",
+            "4",
+            "5",
+            "4",
+            "5",
+            "4",
             "3"
+        ]
+    },
+    {
+        "name": "Gary",
+        "photo": "https://static.comicvine.com/uploads/square_medium/11132/111325030/5880119-spongebob+squarepants+gary+the+snail.jpg",
+        "scores": [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "5",
+            "4",
+            "3",
+            "2",
+            "1"
+        ]
+    },
+    {
+        "name": "Plankton",
+        "photo": "https://res.cloudinary.com/teepublic/image/private/s--SYHzxhpM--/t_Preview/b_rgb:6e2229,c_limit,f_jpg,h_630,q_90,w_630/v1509109320/production/designs/2004374_1.jpg",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Mrs. Puff",
+        "photo": "https://static.comicvine.com/uploads/scale_medium/11132/111325030/5880117-spongebob+squarepants+mrs+puff.jpg",
+        "scores": [
+            "2",
+            "2",
+            "3",
+            "3",
+            "4",
+            "4",
+            "5",
+            "5",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "Pearl Krabs",
+        "photo": "https://vignette.wikia.nocookie.net/spongebob/images/6/69/Pearl_krabs_image.PNG/revision/latest?cb=20171224014357",
+        "scores": [
+            "2",
+            "3",
+            "2",
+            "3",
+            "3",
+            "4",
+            "3",
+            "2",
+            "4",
+            "3"
+        ]
+    },
+    {
+        "name": "Larry the Lobster",
+        "photo": "https://vignette.wikia.nocookie.net/spongebob/images/6/6e/Larry_the_Lobster_in_The_Krabby_Kronicle-16.png/revision/latest?cb=20150201231032",
+        "scores": [
+            "3",
+            "1",
+            "5",
+            "2",
+            "4",
+            "3",
+            "1",
+            "4",
+            "2",
+            "5"
         ]
     },
     
 ]
+//export the array
+module.exports = posFriends;
